6866a8bcb9a003aa6784aa259feaa65d06f443e7
Przemysaw Kolka <przemyslawkolka@gmail.com>
2021-11-22 16:56:36 +0100

    User panel complete, needs minor fix (data sync)

--- src/reducers/actions/mainUserActions.js
+++ src/reducers/actions/mainUserActions.js
@@ -1,15 +1,12 @@
 const mainUserAPI = 'https://randomuser.me/api/?noinfo'
 
-const GET_MAINUSER=(userInfo)=>{
+ const GET_MAINUSER=(userInfo)=>{
     return{
         type: 'GET_MAINUSER',
         userInfo
     }
 }
-
-
 export const fetchMainUserInfo = (dispatch)=>{
-
     fetch(mainUserAPI)
     .then(response=>response.json())
     .then(data=> data.results)
@@ -18,3 +15,10 @@ export const fetchMainUserInfo = (dispatch)=>{
         dispatch(GET_MAINUSER(userInfo))
     })}
 
+export     const SET_MAINUSER_DETAILS=(userInfo)=>{
+    return{
+        type: 'SET_MAINUSER_DETAILS',
+        userInfo
+        
+    }
+}
--- src/reducers/mainUserReducer.js
+++ src/reducers/mainUserReducer.js
@@ -1,15 +1,27 @@
 const mainUserReducer=(state={loaded:false
         
 },action)=>{
+
+    const userInfo=action.userInfo
    
     switch(action.type){
+        case('SET_MAINUSER_DETAILS'):
+
+                state = {
+                loaded:true,
+                userInfo
+                }
+                console.log('w reducerze')
+                console.log(action)
+            return state
         case('GET_MAINUSER'):
-            const userInfo=action.userInfo
+
             state = {
                 loaded:true,
                 userInfo
                 }
             return state
+        
          default:
              return state
              }
