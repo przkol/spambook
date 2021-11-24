0ae8f7e227da682f4444cdfc397edb2c96ff258e
Przemysaw Kolka <przemyslawkolka@gmail.com>
2021-11-24 19:56:44 +0100

    add post functionality implemented

--- src/components/PhotoPost.jsx
+++ src/components/PhotoPost.jsx
@@ -5,9 +5,9 @@ import { Comment } from "./Comment";
 
 
 const PhotoPost = (props)=>{
-const {friend, photo, type,joke,comments, likes,liked,showComments, handleCommentsToggle,handleLike,addComment,} = props
-const friendName = friend?.name ? friend.name.first + ' ' + friend.name.last : 'Jan Kowalski'
-const friendImage=friend?.picture.thumbnail
+const {user,text, photo, type,joke,comments, likes,liked,showComments, handleCommentsToggle,handleLike,addComment} = props
+const userName = user?.name ? user.name.first + ' ' + user.name.last : null
+const userProfilePicture=user?.picture.thumbnail
 const commentsToRender=comments.map((element,index)=><Comment key={index} person={element.person} comment={element.comment}/>)
 const mainUserState = useSelector(state =>state.mainUserReducer)
 const [userComment,setUserComment]=useState('')
@@ -25,12 +25,50 @@ const handleSubmit=(e)=>{
 
 }
 
+if(type==='userPost'){
+    return(
+    <StyledPhotoPost>
+      <div className='postHeader'>
+            <img src={userProfilePicture} alt={userName + `'s profile picture`} /> 
+            <p><span>{userName} </span>{type==='photo' ? 'added a photo!':'added a post!'}</p>
+            </div>  
+        <div className='postContent'>
+            {text ? <p>{text}</p> : null}
+            {photo? <img className='postedPhoto' src={photo} alt='cat'/>:null}
+            </div>
+        <div className='reactions'>
+            <p>{liked? 'You and ' +likes +' other people liked this' :likes +' people liked this' }</p>
+            <p>{comments.length} comments</p>
+            </div>
+        <div className='actionContainer' index={props.index} comments='invisible'>
+            <button className={liked? 'liked':null}onClick={handleLike}>Like!</button>
+            <button onClick={handleCommentsToggle}>Comments</button>
+            </div>
+        <div  className={showComments?'commentSection':'commentSection invisible'}>
+            {commentsToRender}
+            <div className='yourComment'>
+                <div className='commentHeader'>
+                    <img src={mainUserState?.userInfo?.picture?.thumbnail} alt={mainUserState.userInfo?.name + `'s profile picture`} />
+                    <p>{mainUserState.userInfo?.name?.first +' '+ mainUserState.userInfo?.name?.last}</p> 
+                    </div>
+                    <div className='inputContainer'index={props.index}>
+                        <input  onChange={handleChange}  value={userComment} placeholder='Start typing'type='text'/>
+                        <button  onClick={handleSubmit}>Post</button>
+                        </div>
+                </div>
+            </div>
 
+        
+    </StyledPhotoPost>)
+
+}
+
+else if(type==='photo' || type==='joke'){
 return(
-    <StyledPhotoPost >
+    <StyledPhotoPost>
         <div className='postHeader'>
-            <img src={friendImage} alt={friendName + `'s profile picture`} /> 
-            <p><span>{friendName} </span>{type==='photo' ? 'added a photo!':'added a post!'}</p>
+            <img src={userProfilePicture} alt={userName + `'s profile picture`} /> 
+            <p><span>{userName} </span>{type==='photo' ? 'added a photo!':'added a post!'}</p>
             </div>
         <div className='postContent'>
             <p>{type==='photo' ? 'Check out this cute cat!':'HAHA, check this out:'}</p><br/>
@@ -60,7 +98,7 @@ return(
                 </div>
             </div>
     </StyledPhotoPost>
-)
+)}
 
 }
 export default PhotoPost
